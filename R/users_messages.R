62487cd73750f0aaebfec34f3f27ac2479be550c
Wences91 <wences91@gmail.com>
2021-10-11 13:01:26 +0200

    Update users_messages.R

--- R/users_messages.R
+++ R/users_messages.R
@@ -3,14 +3,14 @@
 #' @param messages_dataframe A messages data.frame obtained from the Telegram group fil
 #' @description This function obtaint a data.frame with the users and its number of messages.
 #' @export
-#' @importFrom dplyr group_by summarise arrange desc
+#' @importFrom dplyr group_by summarise arrange desc n
 #' 
 
 users_messages <- function(messages_dataframe){
   
   df_users <- messages_dataframe[,c('from', 'from_id')]
   df_users <- dplyr::group_by(df_users,  from, from_id)
-  df_users <- dplyr::summarise(df_users, messages = n())
+  df_users <- dplyr::summarise(df_users, messages = dplyr::n())
   df_users <- dplyr::arrange(df_users, dplyr::desc(messages))
   
   message(paste(dim(df_users)[1]))
