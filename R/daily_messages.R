1c6dbaf453b0f7acb584d5654423a5348c17e11a
Wences91 <wences91@gmail.com>
2021-10-11 13:48:23 +0200

    Update daily_messages.R

--- R/daily_messages.R
+++ R/daily_messages.R
@@ -1,12 +1,14 @@
 #' daily_messages
 #' 
 #' @param messages_dataframe A messages data.frame obtained from the Telegram group file
+#' @param calendar Boolean, daily messages are grouped by week, month and year
 #' @description This function obtain a data.frame with the number of daily messages.
 #' @export
 #' @importFrom dplyr mutate select group_by summarise n
+#' @importFrom lubridate month
 #' 
 
-daily_messages <- function(messages_dataframe){
+daily_messages <- function(messages_dataframe, calendar=FALSE){
   
   df_dm  <- dplyr::mutate(messages_dataframe, date=as.Date(substr(date, 1, 10), format='%Y-%m-%d'))
   df_dm  <- dplyr::select(df_dm, date)
@@ -23,6 +25,23 @@ daily_messages <- function(messages_dataframe){
                             stringsAsFactors = FALSE)
   
   message(paste(dim(df_dm)[1], 'days since the Telegram group creation'))
+  
+  
+  if(calendar){
+    df_dm$weekday <- as.POSIXlt(df_dm$date)$wday+1
+    df_dm$weekday <- factor(df_dm$weekday,
+                            levels=rev(1:7),
+                            labels=rev(c('Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun')),
+                            ordered=TRUE) 
+    df_dm$month <- factor(lubridate::month(df_dm$date),
+                          levels=as.character(1:12),
+                          labels=c('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'),
+                          ordered=TRUE) 
+    df_dm$year<- factor(substr(df_dm$date,1,4))
+    
+    df_dm <- dplyr::group_by(df_dm, month, weekday, year)
+    df_dm <- dplyr::summarise(df_dm, messages=sum(messages))
+  }
   
   return(df_dm)
   
